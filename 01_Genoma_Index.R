134dc20122f325acfdbf299264d547facc4492bc
Luis Jave-Suarez <92930300+LuisJave@users.noreply.github.com>
2025-03-04 13:33:52 -0600

    01_Genoma_Index.R

--- 01_Genoma_Index.R
+++ 01_Genoma_Index.R
@@ -4,12 +4,9 @@
 # Tener una carpeta con los datos del genoma y los archivos de topología
 # Los archivos se pueden descargar de --> https://www.gencodegenes.org/
 # archivo GTF, GFF3 y Fasta del Genoma de interes
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Cargar la libreria de Rsubread
-
-library(Rsubread)
-
-# crear el directorio "./RefGen47/" en la carpeta de trabajo.
+# Crear las siguientes carpetas:
+# ./RefData/Hg38v47/   <--- carpeta con los archivos del genoma Fasta, GTF y GFF
+# ./RefGen47/          <--- carpeta donde se creará el índice del genoma
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Si los recursos de la computadora son limitados
 # cambiar:
@@ -17,6 +14,8 @@ library(Rsubread)
 # memory = VALOR <-- igual al máximo de la memoria del computador menos 2GB
 # Si la computadora tiene al menos 24GB de memoria se puede dejar como se muestra
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Cargar la libreria de Rsubread
+library(Rsubread)
 
 if (file.exists("./RefData/Hg38v47/GRCh38.primary_assembly.genome.fa.gz"))
 { buildindex (basename = "./RefGen47/Hg38v47",
