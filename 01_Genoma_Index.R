3456007c0ceef94be780b06c0387e185840761f5
Luis Jave-Suarez <92930300+LuisJave@users.noreply.github.com>
2025-10-23 09:52:26 -0600

    Update genome index creation for Hg38 v49

--- 01_Genoma_Index.R
+++ 01_Genoma_Index.R
@@ -1,12 +1,12 @@
 # ============================================================================
-# Crear el Indice del Genoma de Humano Hg38 vXX para Rsubread
+# Crear el Indice del Genoma de Humano Hg38 v49 para Rsubread
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Tener una carpeta con los datos del genoma y los archivos de topología
 # Los archivos se pueden descargar de --> https://www.gencodegenes.org/
-# archivo GTF, GFF3 y Fasta del Genoma de interes
+# archivo GTF, GFF3 y Fasta del genoma de interés
 # Crear las siguientes carpetas:
-# ./RefData/Hg38v47/   <--- carpeta con los archivos del genoma Fasta, GTF y GFF
-# ./RefGen47/          <--- carpeta donde se creará el índice del genoma
+# ./RefData/Hg38v49/   <--- carpeta con los archivos del genoma Fasta, GTF y GFF
+# ./RefGen49/          <--- carpeta donde se creará el índice del genoma
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Si los recursos de la computadora son limitados
 # cambiar:
@@ -14,14 +14,15 @@
 # memory = VALOR <-- igual al máximo de la memoria del computador menos 2GB
 # Si la computadora tiene al menos 24GB de memoria se puede dejar como se muestra
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Cargar la libreria de Rsubread
-library(Rsubread)
+# Cargar la librería de Rsubread
 
-if (file.exists("./RefData/Hg38v47/GRCh38.primary_assembly.genome.fa.gz"))
-{ buildindex (basename = "./RefGen47/Hg38v47",
-             reference = "./RefData/Hg38v47/GRCh38.primary_assembly.genome.fa.gz",
+library(Rsubread)
+if (file.exists("./RefData/Hg38v49/GRCh38.primary_assembly.genome.fa.gz"))
+{ buildindex (basename = "./RefGen49/Hg38v49",
+             reference = "./RefData/Hg38v49/GRCh38.primary_assembly.genome.fa.gz",
              gappedIndex = FALSE,
              indexSplit = FALSE,
              # memory = 10000
              )
 } else { print(paste("El archivo del genoma no se encuentra en la carpeta ./RefData/"))}
+
